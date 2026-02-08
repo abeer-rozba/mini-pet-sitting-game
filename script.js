ca4577728174756a34a2c4ab29711acae895113a
abeer-rozba <abeerrozba3@gmail.com>
2026-02-08 10:35:57 +0300

    redirection works

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const startButton = document.querySelector('#start-game')
 const header = document.querySelector('h1')
 
 startButton.addEventListener('click', () => {
-  console.log('works')
+  window.location = './mainDisplay.html'
 })
