f591510cf5258ef66bfba57ea1c53d9ad5093585
abeer-rozba <abeerrozba3@gmail.com>
2026-02-08 11:37:07 +0300

    create status bars

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const startButton = document.querySelector('#start-game')
 const header = document.querySelector('h1')
+const statusContainer = document.querySelector('.status-container')
 
-startButton.addEventListener('click', () => {
-  window.location = './mainDisplay.html'
-})
+const startGame = () => {
+  console.log('game started')
+  startingSettings()
+}
+
+const startingSettings = () => {
+  header.style.marginBottom = '0px'
+  header.style.visibility = 'hidden'
+  startButton.style.visibility = 'hidden'
+  statusContainer.style.marginTop = '34px'
+  statusContainer.style.visibility = 'visible'
+}
+
+startButton.addEventListener('click', startGame)
