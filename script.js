f51279c7c9a9f75ce4b56401c309fbe7dbc78ad6
abeer-rozba <abeerrozba3@gmail.com>
2026-02-08 10:34:10 +0300

    introductary page complete

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Working..')
+const startButton = document.querySelector('#start-game')
+const header = document.querySelector('h1')
+
+startButton.addEventListener('click', () => {
+  console.log('works')
+})
