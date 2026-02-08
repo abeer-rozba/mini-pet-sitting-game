92247e23f60d211ba5cf82d31e6875d83f01d252
abeer-rozba <abeerrozba3@gmail.com>
2026-02-08 12:58:21 +0300

    create decreasing status bars

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const startButton = document.querySelector('#start-game')
 const header = document.querySelector('h1')
 const statusContainer = document.querySelector('.status-container')
+const statusBars = document.querySelectorAll('.status-bar')
 
 const startGame = () => {
   console.log('game started')
   startingSettings()
+  decreaseStatus()
 }
 
 const startingSettings = () => {
@@ -13,6 +15,40 @@ const startingSettings = () => {
   startButton.style.visibility = 'hidden'
   statusContainer.style.marginTop = '34px'
   statusContainer.style.visibility = 'visible'
+}
+
+const decreaseStatus = () => {
+  let statusBars = document.querySelectorAll('.status-bar')
+  statusBars.forEach((bar) => {
+    let width = 150
+    let duration = 10000
+    let interval = 10
+    let decreaseAmount = width / (duration / interval)
+
+    let timer = setInterval(() => {
+      if (width <= 120) {
+        bar.style.backgroundColor = 'rgb(145, 190, 23)'
+      }
+      if (width <= 90) {
+        bar.style.backgroundColor = 'rgb(215, 195, 45)'
+      }
+      if (width <= 30) {
+        bar.style.backgroundColor = 'rgb(215, 127, 45)'
+      }
+      if (width <= 10) {
+        bar.style.backgroundColor = 'rgb(238, 33, 33)'
+      }
+
+      if (width <= 0) {
+        clearInterval(timer)
+      } else {
+        width -= decreaseAmount
+        bar.style.width = width + 'px'
+      }
+    }, interval)
+
+    setTimeout(() => clearInterval(timer), duration)
+  })
 }
 
 startButton.addEventListener('click', startGame)
